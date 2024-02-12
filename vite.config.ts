cb3f2f42500eda640691621484782bb58b3abe81
z-shaffer <zshaffer@protonmail.com>
2024-02-12 17:31:33 +0100

    Dependency update

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,10 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/tradeRoutes': {
-        target: 'http://localhost:5050', 
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/tradeRoutes/, ''),
-      },
+  build: {
+    manifest: true,
+    rollupOptions: {
+      input: '/backend/server.js',
     },
   },
 });
