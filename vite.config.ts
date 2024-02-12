7f61c8aba735fd1b0898c4ba39e7b2679f6bfa1b
z-shaffer <zshaffer@protonmail.com>
2024-02-12 14:01:43 +0100

    Testing vercel deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/tradeRoutes': {
+        target: 'http://localhost:5050', 
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/tradeRoutes/, ''),
+      },
+    },
+  },
+});
